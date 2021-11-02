d5679f58ebfadd069fd44cc31a44ca70e8ed2847
Стас Миронов <s.mironov@uplab.ru>
2021-11-02 08:25:43 +0300

    ARTELDEV-88 [html, css, js, json] страница Контакты

---
+++ src/js/components/validation/index.js
@@ -0,0 +1,34 @@
+import 'parsleyjs';
+import './parsley-ru';
+
+export default {
+	init() {
+		window.addEventListener('init.validation', () => {
+			const $lang = $('html').attr('lang');
+			const $forms = $(
+				'[data-parsley-validate]:not([data-parsley-initialized])'
+			);
+			$forms.each((index, form) => {
+				const $form = $(form);
+				$form.parsley({
+					errorClass: 'parsley-error',
+					successClass: 'parsley-success',
+				});
+
+				$form.on('submit', (e) => {
+					if (form.hasAttribute('data-ajax-form')) {
+						e.preventDefault();
+
+						window.dispatchEvent(
+							new CustomEvent('form:submit', { detail: e })
+						);
+					}
+				});
+
+				$form.attr('data-parsley-initialized', '');
+				window.Parsley.setLocale(`${$lang}`);
+			});
+		});
+		window.dispatchEvent(new CustomEvent('init.validation'));
+	},
+};
