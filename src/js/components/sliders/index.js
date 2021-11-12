9451b490e61d6a89a53374897f26d2b819a909d9
Стас Миронов <s.mironov@uplab.ru>
2021-11-12 13:08:13 +0300

    ARTELDEV-89 [html, js, css, json] страница Стратегия

--- src/js/components/sliders/index.js
+++ src/js/components/sliders/index.js
@@ -1,12 +1,15 @@
 import SliderMain from './slider-main';
 import SliderInfo from './slider-info';
+import SliderStrategy from './slider-strategy';
 import Tabs from './tabs';
 import './slider-logos';
 
 export default {
 	init() {
+		new SliderStrategy();
 		new SliderMain();
 		new SliderInfo();
+
 		Tabs.init();
 	},
 };
