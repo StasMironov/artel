598270cd2c08b364a8645226337b45ef54243f0c
Стас Миронов <s.mironov@uplab.ru>
2021-12-08 16:05:55 +0300

    ARTELDEV-169 [css, js, json] правки Пресс-центр

--- src/js/components/sliders/slider-press.js
+++ src/js/components/sliders/slider-press.js
@@ -1,71 +1,71 @@
 import { isDesktop } from '../../utils/breakpoints';
 import Slider from './constructor';
 
-const sliderLogos = new Slider({
-	init: true,
-	wrap: '[data-slider-press-wrap]',
-	slider: '[data-slider-press]',
-	prev: '[data-nav-arrow-prev]',
-	next: '[data-nav-arrow-next]',
-	options: {
-		slidesPerView: 'auto',
-		allowTouchMove: true,
-		speed: 1300,
-		watchSlidesVisibility: true,
-		//loop: true,
-		fadeEffect: { crossFade: true },
-		effect: 'fade',
-		lazy: {
-			loadPrevNext: true,
-			elementClass: 'swiper-lazy',
-		},
-		autoHeight: false,
-		breakpoints: {
-			768: {
-				autoHeight: false,
+if (document.querySelector('[data-slider-press-wrap]')) {
+	const pagSlide = new Slider({
+		init: true,
+		wrap: '[data-slider-press-wrap]',
+		slider: '[data-pag]',
+		options: {
+			slidesPerView: 5,
+			spaceBetween: 20,
+			touchRatio: false,
+			//loop: true,
+			slideToClickedSlide: true,
+			touchRatio: 0.2,
+			breakpoints: {
+				359: {
+					spaceBetween: 14,
+					slidesPerView: 3,
+				},
+				641: {
+					slidesPerView: 5,
+				},
+				769: {
+					spaceBetween: 20,
+				},
+			},
+			on: {
+				init() {
+					if (this.slides.length >= 5) {
+						document
+							.querySelector('[data-pag]')
+							.classList.add('overlay');
+					}
+				},
 			},
 		},
-		pagination: {
-			el: '[data-pag]',
-			clickable: true,
-			renderBullet: function (index, className) {
-				return (
-					'<span class="' +
-					className +
-					'">0' +
-					(index + 1) +
-					'</span>'
-				);
+	});
+
+	const sliderPress = new Slider({
+		init: true,
+		wrap: '[data-slider-press-wrap]',
+		slider: '[data-slider-press]',
+		prev: '[data-nav-arrow-prev]',
+		next: '[data-nav-arrow-next]',
+		initialSlide: 0,
+		options: {
+			allowTouchMove: true,
+			speed: 1300,
+			watchSlidesVisibility: true,
+			watchSlidesProgress: true,
+			//centerInsufficientSlides: true,
+			slideToClickedSlide: true,
+			//touchRatio: false,
+			fadeEffect: { crossFade: true },
+			effect: 'fade',
+			loop: true,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'swiper-lazy',
+			},
+			autoHeight: false,
+
+			thumbs: {
+				swiper: pagSlide.swiper,
 			},
 		},
-		on: {
-			init() {
-				if (
-					!!this.slides &&
-					!!this.visibleSlidesIndexes &&
-					this.slides.length <= this.visibleSlidesIndexes.length
-				) {
-					this.wrapperEl
-						.closest('[data-slider-press-wrap]')
-						.classList.add('nav-hidden');
-				}
-			},
-			resize() {
-				if (
-					!!this.slides &&
-					!!this.visibleSlidesIndexes &&
-					this.slides.length <= this.visibleSlidesIndexes.length
-				) {
-					this.wrapperEl
-						.closest('[data-slider-press-wrap]')
-						.classList.add('nav-hidden');
-				} else {
-					this.wrapperEl
-						.closest('[data-slider-press-wrap]')
-						.classList.remove('nav-hidden');
-					this.slideTo(0);
-				}
-			},
-		},
-	},
-});
+	});
+	sliderPress.update();
+	pagSlide.update();
+}
