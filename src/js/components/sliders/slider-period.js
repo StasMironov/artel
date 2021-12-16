671f95def0360f3d3db4ca181b939736eb5c8db6
Стас Миронов <s.mironov@uplab.ru>
2021-12-16 10:34:21 +0300

    ARTELDEV-169 [js, css, html] История

---
+++ src/js/components/sliders/slider-period.js
@@ -0,0 +1,24 @@
+import Slider from './constructor';
+
+const sliderLogos = new Slider({
+	init: true,
+	wrap: '[data-slider-period-wrap]',
+	slider: '[data-slider-period]',
+	options: {
+		slidesPerView: 'auto',
+		allowTouchMove: true,
+		speed: 300,
+		watchSlidesVisibility: true,
+		// loop: true,
+		lazy: {
+			loadPrevNext: true,
+			elementClass: 'swiper-lazy',
+		},
+		autoHeight: true,
+		// breakpoints: {
+		// 	768: {
+		// 		autoHeight: false,
+		// 	},
+		// },
+	},
+});
