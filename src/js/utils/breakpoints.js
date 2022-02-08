26b211200e547b9319c20da70133e6d2693ed57a
Стас Миронов <s.mironov@uplab.ru>
2022-02-08 18:40:50 +0300

    ARTELDEV-169 [js, css] правки Партнёрам

--- src/js/utils/breakpoints.js
+++ src/js/utils/breakpoints.js
@@ -5,8 +5,7 @@ const devices = {
 
 const isDesktop = () =>
 	window.matchMedia(`(min-width: ${devices.lg}px)`).matches;
-const isMob = () =>
-	window.matchMedia(`(max-width: ${devices.md - 1}px)`).matches;
+const isMob = () => window.matchMedia(`(max-width: ${devices.md}px)`).matches;
 const isTablet = () =>
 	window.matchMedia(
 		`(min-width: ${devices.md}px) and (max-width: ${devices.lg - 1}px)`
