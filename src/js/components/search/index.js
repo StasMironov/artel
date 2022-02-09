94e5d7a1ccc12ead14461d694385236e6daf6303
Стас Миронов <s.mironov@uplab.ru>
2022-02-09 12:08:01 +0300

    ARTELDEV-169 [css, js] правки

--- src/js/components/search/index.js
+++ src/js/components/search/index.js
@@ -1,5 +1,5 @@
-import field from "parsleyjs/src/parsley/field";
-import {throttle} from "throttle-debounce";
+import field from 'parsleyjs/src/parsley/field';
+import { throttle } from 'throttle-debounce';
 import PerfectScrollbar from 'perfect-scrollbar';
 
 export default class Search {
@@ -27,24 +27,30 @@ export default class Search {
 			e.preventDefault(); // Страницы с выдачей не будет
 		});
 
-		this.field.addEventListener('keyup', throttle(200, false, (e) => {
-			const value = e.target.value;
+		this.field.addEventListener(
+			'keyup',
+			throttle(200, false, (e) => {
+				const value = e.target.value;
 
-			if (value.trim().length > 1) {
-				this.fetch();
-				this.form.classList.add('is-active');
-			} else {
-				this.appendText();
-				this.form.classList.remove('is-active');
-			}
-		}));
+				if (value.trim().length > 1) {
+					this.fetch();
+					this.form.classList.add('is-active');
+				} else {
+					this.appendText();
+					this.form.classList.remove('is-active');
+				}
+			})
+		);
 
 		this.reset.addEventListener('click', () => {
 			this.appendText();
 		});
 
 		document.addEventListener('click', (e) => {
-			if (!e.target.hasAttribute('data-search-ajax') && !e.target.closest('[data-search-ajax]')) {
+			if (
+				!e.target.hasAttribute('data-search-ajax') &&
+				!e.target.closest('[data-search-ajax]')
+			) {
 				this.form.classList.remove('is-active');
 			}
 		});
@@ -54,30 +60,32 @@ export default class Search {
 		const formData = new FormData(this.form);
 		let url = this.url;
 
-		url +=
-			[
-				url.indexOf('?') >= 0 ? '&' : '?',
-				`query=${formData.get('search')}`
-			].join('');
+		url += [
+			url.indexOf('?') >= 0 ? '&' : '?',
+			`q=${formData.get('search')}`,
+		].join('');
 
 		const response = fetch(url);
 
-		response.then((res) => {
-			if (res.ok) {
-				return res.text();
-			} else {
-				throw new Error('Something has gone wrong :(')
-			}
-		}).then((text) => {
-			this.appendText(text);
-		}).catch((err) => {
-			console.log(`Failed to fetch url (${url}): `, err);
-		});
+		response
+			.then((res) => {
+				if (res.ok) {
+					return res.text();
+				} else {
+					throw new Error('Something has gone wrong :(');
+				}
+			})
+			.then((text) => {
+				this.appendText(text);
+			})
+			.catch((err) => {
+				console.log(`Failed to fetch url (${url}): `, err);
+			});
 	}
 
 	appendText(value = null) {
 		if (!value) {
-			this.frame.innerHTML = "";
+			this.frame.innerHTML = '';
 		} else {
 			this.frame.innerHTML = value;
 			this.initPs();
