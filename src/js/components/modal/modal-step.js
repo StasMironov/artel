c33e9ba94e779f712818189a61e691f52aa8e1de
Стас Миронов <s.mironov@uplab.ru>
2022-02-14 08:27:03 +0300

    MironovSDev [js, css] popup

--- src/js/components/modal/modal-step.js
+++ src/js/components/modal/modal-step.js
@@ -1,13 +1,29 @@
+
+
+import libs from '../../libs';
+import select from '../select';
+
 export default {
 	init() {
 		const wrapperNode = document.querySelector('[data-steps-career]');
-		console.log(wrapperNode);
+	
 		if (!wrapperNode) false;
 
 		$(wrapperNode).children('div').steps({
 			headerTag: 'h3',
 			bodyTag: 'section',
 			transitionEffect: 'slideLeft',
+			labels: {
+				finish: "Отправить",
+				next: "Дальше",
+				previous: "Назад",
+			},
+			onInit: ()=> {
+				libs.init();
+				select.init();
+			}
 		});
+
+		
 	},
 };
